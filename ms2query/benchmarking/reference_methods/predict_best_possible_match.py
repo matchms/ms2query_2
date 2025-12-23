39b541406291a5dfdcec0f6af74ac5a863736272
niekdejonge <niekdejonge@gmail.com>
2025-12-23 15:47:16 +0100

    Add fingerprints as input to predict_best_possible_match.py

--- ms2query/benchmarking/reference_methods/predict_best_possible_match.py
+++ ms2query/benchmarking/reference_methods/predict_best_possible_match.py
@@ -1,11 +1,13 @@
 from typing import Dict
-import numpy as np
 from matchms.similarity.vector_similarity_functions import jaccard_similarity_matrix
 from ms2query.benchmarking.AnnotatedSpectrumSet import AnnotatedSpectrumSet
+from ms2query.benchmarking.Fingerprints import Fingerprints
 
 
-def predict_best_possible_match(library_spectra: AnnotatedSpectrumSet, query_spectra: AnnotatedSpectrumSet):
-    highest_possible_score_per_inchikey = calculate_highest_tanimoto_score_per_inchikey(library_spectra, query_spectra)
+def predict_best_possible_match(library_spectra: AnnotatedSpectrumSet,
+                                query_spectra: AnnotatedSpectrumSet,
+                                fingerprints: Fingerprints,):
+    highest_possible_score_per_inchikey = calculate_highest_tanimoto_score_per_inchikey(library_spectra, query_spectra, fingerprints)
 
     inchikeys_of_best_match = []
     highest_scores = []
@@ -20,26 +22,31 @@ def predict_best_possible_match(library_spectra: AnnotatedSpectrumSet, query_spe
 
 
 def calculate_highest_tanimoto_score_per_inchikey(
-    library_spectra: AnnotatedSpectrumSet, query_spectra: AnnotatedSpectrumSet
+    library_spectra: AnnotatedSpectrumSet, query_spectra: AnnotatedSpectrumSet,
+        fingerprints: Fingerprints
 ) -> Dict[str, tuple[str, float]]:
     """Finds the best possible match during an analogue search"""
     print("Calculating tanimoto scores to determine best possible match")
 
-    library_fingerprints = library_spectra.fingerprints.fingerprints
-    query_fingerprints = query_spectra.fingerprints.fingerprints
+
+    library_fingerprints = fingerprints.get_fingerprints(library_spectra.inchikeys)
+    query_fingerprints = fingerprints.get_fingerprints(query_spectra.inchikeys)
+
     tanimoto_scores = jaccard_similarity_matrix(library_fingerprints, query_fingerprints)
     highest_scores = tanimoto_scores.max(axis=0, initial=0)
     indexes_of_highest_scores = tanimoto_scores.argmax(axis=0)
 
     highest_possible_score_per_inchikey = dict()
-    for i, inchikey in enumerate(query_spectra.fingerprints.inchikeys):
+    # todo replace with TopKTanimotoScores
+    inchikeys_in_library = set(library_spectra.inchikeys())
+    for i, inchikey in enumerate(query_spectra.inchikeys):
         # Check if inchikey in library (To correctly handle the exact matching case)
-        if inchikey in library_spectra.fingerprints.inchikeys:
+        if inchikey in inchikeys_in_library:
             highest_possible_score_per_inchikey[inchikey] = (inchikey, 1.0)
             continue
 
         highest_possible_score_per_inchikey[inchikey] = (
-            library_spectra.fingerprints.inchikeys[indexes_of_highest_scores[i]],
+            library_spectra.inchikeys[indexes_of_highest_scores[i]],
             highest_scores[i],
         )
     return highest_possible_score_per_inchikey
