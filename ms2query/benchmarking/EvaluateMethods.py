e1ddd1bb32bdb29dff8810f768af008f550b82cc
niekdejonge <niekdejonge@gmail.com>
2025-12-23 16:01:39 +0100

    Organize methods for within and across ionmodes

--- ms2query/benchmarking/EvaluateMethods.py
+++ ms2query/benchmarking/EvaluateMethods.py
@@ -11,7 +11,7 @@ class EvaluateExactMatchSearchAcrossIonmodes:
     ):
         self.training_spectrum_set = training_spectrum_set
         (self.pos_validation_spectra,
-         self.neg_validation_spectra) = split_spectrum_set_per_inchikey_across_ionmodes(validation_spectrum_set)
+         self.neg_validation_spectra) = self.split_spectrum_set_per_inchikey_across_ionmodes(validation_spectrum_set)
 
     def pos_in_neg(self, prediction_function: Callable[
             [AnnotatedSpectrumSet, AnnotatedSpectrumSet], Tuple[List[str], List[float]]
@@ -26,16 +26,51 @@ class EvaluateExactMatchSearchAcrossIonmodes:
                                         prediction_function(self.training_spectrum_set + self.pos_validation_spectra,
                                                             self.neg_validation_spectra))
 
+    @staticmethod
+    def split_spectrum_set_per_inchikey_across_ionmodes(self,
+        spectrum_set: AnnotatedSpectrumSet,
+    ) -> Tuple[AnnotatedSpectrumSet, AnnotatedSpectrumSet]:
+        """Splits a spectrum set in two sets on ionmode. Only uses spectra for inchikeys with at least 1 pos and 1 neg"""
+        all_pos_indexes = []
+        all_neg_indexes = []
+        for inchikey in tqdm(
+            spectrum_set.spectrum_indexes_per_inchikey.keys(),
+            desc="Splitting spectra per inchikey across ionmodes",
+        ):
+            val_spectrum_indexes_matching_inchikey = spectrum_set.spectrum_indexes_per_inchikey[inchikey]
+            positive_val_spectrum_indexes_current_inchikey = []
+            negative_val_spectrum_indexes_current_inchikey = []
+            for spectrum_index in val_spectrum_indexes_matching_inchikey:
+                ionmode = spectrum_set.spectra[spectrum_index].get("ionmode")
+                if ionmode == "positive":
+                    positive_val_spectrum_indexes_current_inchikey.append(spectrum_index)
+                elif ionmode == "negative":
+                    negative_val_spectrum_indexes_current_inchikey.append(spectrum_index)
+
+            if (
+                len(positive_val_spectrum_indexes_current_inchikey) < 1
+                or len(negative_val_spectrum_indexes_current_inchikey) < 1
+            ):
+                continue
+            else:
+                all_pos_indexes.extend(positive_val_spectrum_indexes_current_inchikey)
+                all_neg_indexes.extend(negative_val_spectrum_indexes_current_inchikey)
+
+        pos_val_spectra = spectrum_set.subset_spectra(all_pos_indexes)
+        neg_val_spectra = spectrum_set.subset_spectra(all_neg_indexes)
+        return pos_val_spectra, neg_val_spectra
+
+
 class EvaluateExactMatchSearchWithinIonmodes:
     def __init__(
             self, training_spectrum_set: AnnotatedSpectrumSet,
             validation_spectrum_set: AnnotatedSpectrumSet,
     ):
         self.training_spectrum_set = training_spectrum_set
-        self.pos_split_per_inchikey_set_1, self.pos_split_per_inchikey_set_2 = split_spectrum_set_per_inchikeys(
+        self.pos_split_per_inchikey_set_1, self.pos_split_per_inchikey_set_2 = self._split_spectrum_set_per_inchikeys(
             subset_spectra_on_ionmode(validation_spectrum_set, "positive"))
 
-        self.neg_split_per_inchikey_set_1, self.neg_split_per_inchikey_set_2 = split_spectrum_set_per_inchikeys(
+        self.neg_split_per_inchikey_set_1, self.neg_split_per_inchikey_set_2 = self._split_spectrum_set_per_inchikeys(
             subset_spectra_on_ionmode(validation_spectrum_set, "negative"))
 
     def neg_in_neg(self, prediction_function: Callable[
@@ -64,6 +99,25 @@ class EvaluateExactMatchSearchWithinIonmodes:
                                 self.pos_split_per_inchikey_set_1))
         return (accuracy_set_2 + accuracy_set_1) / 2
 
+    @staticmethod
+    def _split_spectrum_set_per_inchikeys(spectrum_set: AnnotatedSpectrumSet,
+                                         seed=42) -> Tuple[AnnotatedSpectrumSet, AnnotatedSpectrumSet]:
+        """Splits a spectrum set into two.
+        For each inchikey with more than one spectrum the spectra are divided over the two sets"""
+        indexes_set_1 = []
+        indexes_set_2 = []
+        rng = random.Random(seed)
+        for inchikey in tqdm(spectrum_set.spectrum_indexes_per_inchikey.keys(), desc="Splitting spectra per inchikey"):
+            val_spectrum_indexes_matching_inchikey = spectrum_set.spectrum_indexes_per_inchikey[inchikey]
+            if len(val_spectrum_indexes_matching_inchikey) == 1:
+                # all single spectra are excluded from this test, since no exact match can be added to the library
+                continue
+            split_index = len(val_spectrum_indexes_matching_inchikey) // 2
+            rng.shuffle(list(val_spectrum_indexes_matching_inchikey))
+            indexes_set_1.extend(val_spectrum_indexes_matching_inchikey[:split_index])
+            indexes_set_2.extend(val_spectrum_indexes_matching_inchikey[split_index:])
+        return spectrum_set.subset_spectra(indexes_set_1), spectrum_set.subset_spectra(indexes_set_2)
+
 def get_exact_match_accuracy(query_spectrum_set, predicted_inchikeys):
     exact_match_accuracy_per_inchikey = []
     for inchikey in tqdm(query_spectrum_set.inchikeys, desc="Calculating exact match accuracy per inchikey"):
@@ -74,59 +128,6 @@ def get_exact_match_accuracy(query_spectrum_set, predicted_inchikeys):
                 correctly_predicted += 1
         exact_match_accuracy_per_inchikey.append(correctly_predicted / len(val_spectrum_indexes_matching_inchikey))
     return sum(exact_match_accuracy_per_inchikey) / len(exact_match_accuracy_per_inchikey)
-
-
-def split_spectrum_set_per_inchikeys(spectrum_set: AnnotatedSpectrumSet,
-                                     seed=42) -> Tuple[AnnotatedSpectrumSet, AnnotatedSpectrumSet]:
-    """Splits a spectrum set into two.
-    For each inchikey with more than one spectrum the spectra are divided over the two sets"""
-    indexes_set_1 = []
-    indexes_set_2 = []
-    rng = random.Random(seed)
-    for inchikey in tqdm(spectrum_set.spectrum_indexes_per_inchikey.keys(), desc="Splitting spectra per inchikey"):
-        val_spectrum_indexes_matching_inchikey = spectrum_set.spectrum_indexes_per_inchikey[inchikey]
-        if len(val_spectrum_indexes_matching_inchikey) == 1:
-            # all single spectra are excluded from this test, since no exact match can be added to the library
-            continue
-        split_index = len(val_spectrum_indexes_matching_inchikey) // 2
-        rng.shuffle(list(val_spectrum_indexes_matching_inchikey))
-        indexes_set_1.extend(val_spectrum_indexes_matching_inchikey[:split_index])
-        indexes_set_2.extend(val_spectrum_indexes_matching_inchikey[split_index:])
-    return spectrum_set.subset_spectra(indexes_set_1), spectrum_set.subset_spectra(indexes_set_2)
-
-
-def split_spectrum_set_per_inchikey_across_ionmodes(
-    spectrum_set: AnnotatedSpectrumSet,
-) -> Tuple[AnnotatedSpectrumSet, AnnotatedSpectrumSet]:
-    """Splits a spectrum set in two sets on ionmode. Only uses spectra for inchikeys with at least 1 pos and 1 neg"""
-    all_pos_indexes = []
-    all_neg_indexes = []
-    for inchikey in tqdm(
-        spectrum_set.spectrum_indexes_per_inchikey.keys(),
-        desc="Splitting spectra per inchikey across ionmodes",
-    ):
-        val_spectrum_indexes_matching_inchikey = spectrum_set.spectrum_indexes_per_inchikey[inchikey]
-        positive_val_spectrum_indexes_current_inchikey = []
-        negative_val_spectrum_indexes_current_inchikey = []
-        for spectrum_index in val_spectrum_indexes_matching_inchikey:
-            ionmode = spectrum_set.spectra[spectrum_index].get("ionmode")
-            if ionmode == "positive":
-                positive_val_spectrum_indexes_current_inchikey.append(spectrum_index)
-            elif ionmode == "negative":
-                negative_val_spectrum_indexes_current_inchikey.append(spectrum_index)
-
-        if (
-            len(positive_val_spectrum_indexes_current_inchikey) < 1
-            or len(negative_val_spectrum_indexes_current_inchikey) < 1
-        ):
-            continue
-        else:
-            all_pos_indexes.extend(positive_val_spectrum_indexes_current_inchikey)
-            all_neg_indexes.extend(negative_val_spectrum_indexes_current_inchikey)
-
-    pos_val_spectra = spectrum_set.subset_spectra(all_pos_indexes)
-    neg_val_spectra = spectrum_set.subset_spectra(all_neg_indexes)
-    return pos_val_spectra, neg_val_spectra
 
 
 def subset_spectra_on_ionmode(spectrum_set: AnnotatedSpectrumSet, ionmode) -> AnnotatedSpectrumSet:
