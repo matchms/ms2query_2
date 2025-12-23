56e3a8bdefdd42617042c29ec13677c6b1417e86
niekdejonge <niekdejonge@gmail.com>
2025-12-23 15:48:25 +0100

    Refactor EvaluateMethods and split on analogue search benchmarking and exact match benchmarking

--- ms2query/benchmarking/EvaluateMethods.py
+++ ms2query/benchmarking/EvaluateMethods.py
@@ -4,31 +4,35 @@ import numpy as np
 from matchms.similarity.vector_similarity_functions import jaccard_similarity_matrix
 from tqdm import tqdm
 from ms2query.benchmarking.AnnotatedSpectrumSet import AnnotatedSpectrumSet
+from ms2query.benchmarking.Fingerprints import Fingerprints
 
-
-class EvaluateMethods:
+class EvaluateAnalogueSearch:
     def __init__(
-        self, training_spectrum_set: AnnotatedSpectrumSet, validation_spectrum_set: AnnotatedSpectrumSet
+            self, training_spectrum_set: AnnotatedSpectrumSet,
+            validation_spectrum_set: AnnotatedSpectrumSet,
+            fingerprint_type="daylight",
+            nbits=2048,
     ):
         self.training_spectrum_set = training_spectrum_set
         self.validation_spectrum_set = validation_spectrum_set
 
+        self.fingerprints = Fingerprints.from_spectrum_set(training_spectrum_set + validation_spectrum_set,
+                                                           fingerprint_type, nbits)
         self.training_spectrum_set.progress_bars = False
         self.validation_spectrum_set.progress_bars = False
 
     def benchmark_analogue_search(
-        self,
-        prediction_function: Callable[
-            [AnnotatedSpectrumSet, AnnotatedSpectrumSet], Tuple[List[str], List[float]]
-        ],
+            self, predicted_inchikeys: List[str],
     ) -> float:
-        predicted_inchikeys, _ = prediction_function(self.training_spectrum_set, self.validation_spectrum_set)
-        average_scores_per_inchikey = []
+        """Calculates the average accuracy of predictions made.
 
+        predicted_inchikeys should match the order in self.validation_spectrum_set
+        """
+        average_scores_per_inchikey = []
         # Calculate score per unique inchikey
         for inchikey in tqdm(
-            self.validation_spectrum_set.spectrum_indexes_per_inchikey.keys(),
-            desc="Calculating analogue accuracy per inchikey",
+                self.validation_spectrum_set.spectrum_indexes_per_inchikey.keys(),
+                desc="Calculating analogue accuracy per inchikey",
         ):
             matching_spectrum_indexes = self.validation_spectrum_set.spectrum_indexes_per_inchikey[inchikey]
             prediction_scores = []
@@ -37,8 +41,8 @@ class EvaluateMethods:
                 if predicted_inchikey is None:
                     prediction_scores.append(0.0)
                 else:
-                    predicted_fingerprint = self.training_spectrum_set.fingerprints.get_fingerprints([predicted_inchikey])[0]
-                    actual_fingerprint = self.validation_spectrum_set.fingerprints.get_fingerprints([inchikey])[0]
+                    predicted_fingerprint = self.fingerprints.get_fingerprints([predicted_inchikey])[0]
+                    actual_fingerprint = self.fingerprints.get_fingerprints([inchikey])[0]
                     tanimoto_for_prediction = calculate_tanimoto_score_between_pair(
                         predicted_fingerprint, actual_fingerprint
                     )
@@ -49,49 +53,6 @@ class EvaluateMethods:
             average_scores_per_inchikey.append(score)
         average_over_all_inchikeys = sum(average_scores_per_inchikey) / len(average_scores_per_inchikey)
         return average_over_all_inchikeys
-
-    def benchmark_exact_matching_within_ionmode(
-        self,
-        prediction_function: Callable[
-            [AnnotatedSpectrumSet, AnnotatedSpectrumSet], Tuple[List[str], List[float]]
-        ],
-        ionmode: str,
-    ) -> float:
-        """Test the accuracy at retrieving exact matches from the library
-
-        For each inchikey with more than 1 spectrum the spectra are split in two sets. Half for each inchikey is added
-        to the library (training set), for the other half predictions are made. Thereby there is always an exact match
-        avaialable. Only the highest ranked prediction is considered correct if the correct inchikey is predicted.
-        An accuracy per inchikey is calculated followed by calculating the average.
-        """
-        selected_spectra = subset_spectra_on_ionmode(self.validation_spectrum_set, ionmode)
-
-        set_1, set_2 = split_spectrum_set_per_inchikeys(selected_spectra)
-
-        predicted_inchikeys = predict_between_two_sets(self.training_spectrum_set, set_1, set_2, prediction_function)
-
-        # add the spectra to set_1
-        set_1.add_spectra(set_2)
-        return calculate_average_exact_match_accuracy(set_1, predicted_inchikeys)
-
-    def exact_matches_across_ionization_modes(
-        self,
-        prediction_function: Callable[
-            [AnnotatedSpectrumSet, AnnotatedSpectrumSet], Tuple[List[str], List[float]]
-        ],
-    ):
-        """Test the accuracy at retrieving exact matches from the library if only available in other ionisation mode
-
-        Each val spectrum is matched against the training set with the other val spectra of the same inchikey, but other
-         ionisation mode added to the library.
-        """
-        pos_set, neg_set = split_spectrum_set_per_inchikey_across_ionmodes(self.validation_spectrum_set)
-        predicted_inchikeys = predict_between_two_sets(
-            self.training_spectrum_set, pos_set, neg_set, prediction_function
-        )
-        # add the spectra to set_1
-        pos_set.add_spectra(neg_set)
-        return calculate_average_exact_match_accuracy(pos_set, predicted_inchikeys)
 
     def get_accuracy_recall_curve(self):
         """This method should test the recall accuracy balance.
@@ -105,32 +66,65 @@ class EvaluateMethods:
         """
         raise NotImplementedError
 
-
-def predict_between_two_sets(
-    library: AnnotatedSpectrumSet, query_set_1: AnnotatedSpectrumSet, query_set_2: AnnotatedSpectrumSet, prediction_function
-):
-    """Makes predictions between query sets and the library, with the other query set added.
-
-    This is necessary for testing exact matching"""
-    training_set_copy = library.copy()
-    training_set_copy.add_spectra(query_set_2)
-    predicted_inchikeys_1, _ = prediction_function(training_set_copy, query_set_1)
-
-    training_set_copy = library.copy()
-    training_set_copy.add_spectra(query_set_1)
-    predicted_inchikeys_2, _ = prediction_function(training_set_copy, query_set_2)
-
-    return predicted_inchikeys_1 + predicted_inchikeys_2
-
-
-def calculate_average_exact_match_accuracy(spectrum_set: AnnotatedSpectrumSet, predicted_inchikeys: List[str]):
-    if len(spectrum_set.spectra) != len(predicted_inchikeys):
-        raise ValueError("The number of spectra should be equal to the number of predicted inchikeys ")
-    exact_match_accuracy_per_inchikey = []
-    for inchikey in tqdm(
-        spectrum_set.spectrum_indexes_per_inchikey.keys(), desc="Calculating exact match accuracy per inchikey"
+class EvaluateExactMatchSearch:
+    def __init__(
+        self, training_spectrum_set: AnnotatedSpectrumSet,
+            validation_spectrum_set: AnnotatedSpectrumSet,
     ):
-        val_spectrum_indexes_matching_inchikey = spectrum_set.spectrum_indexes_per_inchikey[inchikey]
+        self.training_spectrum_set = training_spectrum_set
+        (self.pos_validation_spectra,
+         self.neg_validation_spectra) = split_spectrum_set_per_inchikey_across_ionmodes(validation_spectrum_set)
+
+        self.pos_split_per_inchikey_set_1, self.pos_split_per_inchikey_set_2 = split_spectrum_set_per_inchikeys(
+            self.pos_validation_spectra)
+
+        self.neg_split_per_inchikey_set_1, self.neg_split_per_inchikey_set_2 = split_spectrum_set_per_inchikeys(
+            self.neg_validation_spectra)
+
+    def pos_in_neg(self, prediction_function: Callable[
+            [AnnotatedSpectrumSet, AnnotatedSpectrumSet], Tuple[List[str], List[float]]
+        ],):
+        return get_exact_match_accuracy(self.pos_validation_spectra,
+                                        prediction_function(self.training_spectrum_set + self.neg_validation_spectra,
+                                                            self.pos_validation_spectra))
+    def neg_in_pos(self, prediction_function: Callable[
+            [AnnotatedSpectrumSet, AnnotatedSpectrumSet], Tuple[List[str], List[float]]
+        ],):
+        return get_exact_match_accuracy(self.neg_validation_spectra,
+                                        prediction_function(self.training_spectrum_set + self.pos_validation_spectra,
+                                                            self.neg_validation_spectra))
+
+    def neg_in_neg(self, prediction_function: Callable[
+            [AnnotatedSpectrumSet, AnnotatedSpectrumSet], Tuple[List[str], List[float]]
+        ],):
+        accuracy_set_2 = get_exact_match_accuracy(
+            self.neg_split_per_inchikey_set_2,
+            prediction_function(self.training_spectrum_set + self.neg_split_per_inchikey_set_1,
+                                self.neg_split_per_inchikey_set_2))
+        accuracy_set_1 = get_exact_match_accuracy(
+            self.neg_split_per_inchikey_set_1,
+            prediction_function(self.training_spectrum_set + self.neg_split_per_inchikey_set_2,
+                                self.neg_split_per_inchikey_set_1))
+        return (accuracy_set_2 + accuracy_set_1) / 2
+
+    def pos_in_pos(self, prediction_function: Callable[
+            [AnnotatedSpectrumSet, AnnotatedSpectrumSet], Tuple[List[str], List[float]]
+        ],):
+        accuracy_set_2 = get_exact_match_accuracy(
+            self.pos_split_per_inchikey_set_2,
+            prediction_function(self.training_spectrum_set + self.pos_split_per_inchikey_set_1,
+                                self.pos_split_per_inchikey_set_2))
+        accuracy_set_1 = get_exact_match_accuracy(
+            self.pos_split_per_inchikey_set_1,
+            prediction_function(self.training_spectrum_set + self.pos_split_per_inchikey_set_2,
+                                self.pos_split_per_inchikey_set_1))
+
+        return (accuracy_set_2 + accuracy_set_1) / 2
+
+def get_exact_match_accuracy(query_spectrum_set, predicted_inchikeys):
+    exact_match_accuracy_per_inchikey = []
+    for inchikey in tqdm(query_spectrum_set.inchikeys, desc="Calculating exact match accuracy per inchikey"):
+        val_spectrum_indexes_matching_inchikey = query_spectrum_set.spectrum_indexes_per_inchikey[inchikey]
         correctly_predicted = 0
         for selected_spectrum_idx in val_spectrum_indexes_matching_inchikey:
             if inchikey == predicted_inchikeys[selected_spectrum_idx]:
@@ -139,18 +133,20 @@ def calculate_average_exact_match_accuracy(spectrum_set: AnnotatedSpectrumSet, p
     return sum(exact_match_accuracy_per_inchikey) / len(exact_match_accuracy_per_inchikey)
 
 
-def split_spectrum_set_per_inchikeys(spectrum_set: AnnotatedSpectrumSet) -> Tuple[AnnotatedSpectrumSet, AnnotatedSpectrumSet]:
+def split_spectrum_set_per_inchikeys(spectrum_set: AnnotatedSpectrumSet,
+                                     seed=42) -> Tuple[AnnotatedSpectrumSet, AnnotatedSpectrumSet]:
     """Splits a spectrum set into two.
     For each inchikey with more than one spectrum the spectra are divided over the two sets"""
     indexes_set_1 = []
     indexes_set_2 = []
+    rng = random.Random(seed)
     for inchikey in tqdm(spectrum_set.spectrum_indexes_per_inchikey.keys(), desc="Splitting spectra per inchikey"):
         val_spectrum_indexes_matching_inchikey = spectrum_set.spectrum_indexes_per_inchikey[inchikey]
         if len(val_spectrum_indexes_matching_inchikey) == 1:
             # all single spectra are excluded from this test, since no exact match can be added to the library
             continue
         split_index = len(val_spectrum_indexes_matching_inchikey) // 2
-        random.shuffle(val_spectrum_indexes_matching_inchikey)
+        rng.shuffle(list(val_spectrum_indexes_matching_inchikey))
         indexes_set_1.extend(val_spectrum_indexes_matching_inchikey[:split_index])
         indexes_set_2.extend(val_spectrum_indexes_matching_inchikey[split_index:])
     return spectrum_set.subset_spectra(indexes_set_1), spectrum_set.subset_spectra(indexes_set_2)
