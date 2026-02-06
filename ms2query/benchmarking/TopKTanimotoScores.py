f40f781e9383387c62e81d26f43bf44e8507afa4
niekdejonge <niekdejonge@gmail.com>
2026-02-06 18:26:14 +0100

    Update TopKTanimotoScores

--- ms2query/benchmarking/TopKTanimotoScores.py
+++ ms2query/benchmarking/TopKTanimotoScores.py
@@ -8,15 +8,18 @@ class TopKTanimotoScores:
     def __init__(
         self, tanimoto_scores_for_top_k: np.ndarray, top_k_inchikeys: np.ndarray, inchikey_indexes: np.ndarray
     ):
+        """Stores the top k scores between two lists of inchikeys"""
 
         self.k = tanimoto_scores_for_top_k.shape[1]
-        self.top_k_inchikeys_and_scores = self.create_multi_index(
+        self.top_k_inchikeys_and_scores = self._create_multi_index(
             tanimoto_scores_for_top_k, top_k_inchikeys, inchikey_indexes
         )
 
-    def create_multi_index(
+    def _create_multi_index(
         self, tanimoto_scores_for_top_k: np.ndarray, top_k_inchikeys: np.ndarray, inchikey_indexes: np.ndarray
     ):
+        """Creates a pandas dataframe with multi index.
+        For each inchikey the top ten highest inchikeys and the corresponding score are stored."""
         columns = pd.MultiIndex.from_product(
             [[f"Rank_{i + 1}" for i in range(self.k)], ["inchikey", "score"]], names=["result_rank", "attribute"]
         )
@@ -35,21 +38,31 @@ class TopKTanimotoScores:
             query_fingerprints.fingerprints, target_fingerprints.fingerprints
         )
         inchikey_indexes_of_top_k = np.argpartition(similarity_scores, -k, axis=1)[:, -k:]
-        top_k_inchikeys = target_fingerprints.inchikeys[inchikey_indexes_of_top_k]
+        top_k_inchikeys = np.array(target_fingerprints.inchikeys)[inchikey_indexes_of_top_k]
         tanimoto_scores_for_top_k = similarity_scores[
             np.arange(similarity_scores.shape[0])[:, None], inchikey_indexes_of_top_k
         ]
-        return cls(tanimoto_scores_for_top_k, top_k_inchikeys, query_fingerprints.inchikeys)
+        return cls(tanimoto_scores_for_top_k, top_k_inchikeys, np.array(query_fingerprints.inchikeys))
 
-    def select_top_k_inchikeys_and_scores(self, inchikey):
-        """Returns a DF with inchikeys and scores"""
-        return self.top_k_inchikeys_and_scores.loc[inchikey].unstack(level="result_rank").T
+    def select_top_k_inchikeys_and_scores(self, inchikey) -> dict[str, float]:
+        """Returns a dictionary with inchikeys and scores for the given inchikey"""
+        # Select row with results
+        top_k_inchikeys_and_scores = self.top_k_inchikeys_and_scores.loc[inchikey]
+        # Unstack the multi index to get a dataframe with inchikeys and scores and index rank_1, rank_2 etc.
+        top_k_inchikeys_and_scores = top_k_inchikeys_and_scores.unstack(level="result_rank").T
+        # Convert to dictionary with inchikey, score pairs.
+        return dict(zip(top_k_inchikeys_and_scores["inchikey"], top_k_inchikeys_and_scores["score"]))
 
-    def select_top_k_inchikeys(self, inchikey):
-        return self.top_k_inchikeys_and_scores.loc[inchikey].xs("inchikey", level="attribute")
+    def select_top_k_inchikeys(self, inchikey) -> list[float]:
+        return list(self.top_k_inchikeys_and_scores.loc[inchikey].xs("inchikey", level="attribute"))
 
-    def select_average_score(self, inchikey):
-        return self.top_k_inchikeys_and_scores.loc[inchikey].xs("score", level="attribute").mean()
+    def select_average_score(self, inchikey) -> float:
+        """Returns the average tanimoto score for the top k highest scores for the specified inchikey"""
+        return float(self.top_k_inchikeys_and_scores.loc[inchikey].xs("score", level="attribute").mean())
 
-    def get_all_average_tanimoto_scores(self):
-        return self.top_k_inchikeys_and_scores.xs("score", axis=1, level="attribute").mean(axis=1)
+    def get_all_average_tanimoto_scores(self) -> dict[str, float]:
+        """Returns all average tanimoto scores for the top k per inchikey"""
+        average_per_inchikey_df = self.top_k_inchikeys_and_scores.xs("score", axis=1, level="attribute").mean(axis=1)  # type: ignore
+        # convert to dictionary
+        average_per_inchikey = average_per_inchikey_df.squeeze().to_dict()
+        return average_per_inchikey
