8108df6b8b81b269a2727b3e48bef0ec3fca544a
niekdejonge <niekdejonge@gmail.com>
2026-02-11 10:08:22 +0100

    First test for MS2DeepScoresForTopinchikeys

---
+++ tests/test_benchmarking/test_MS2DeepScoresForTopInChikeys.py
@@ -0,0 +1,14 @@
+from ms2query.benchmarking.AnnotatedSpectrumSet import AnnotatedSpectrumSet
+from ms2query.benchmarking.reference_methods.MS2DeepScoresForTopInChikeys import (
+    calculate_MS2DeepScoresForTopKInChikeys_from_spectra,
+)
+from tests.helper_functions import create_test_spectra, ms2deepscore_model
+
+
+def test_calculate_MS2DeepScoresForTopKInChikeys_from_spectra():
+    model = ms2deepscore_model()
+    library_spectra = AnnotatedSpectrumSet.create_spectrum_set(create_test_spectra(nr_of_inchikeys=7))
+    test_spectra = AnnotatedSpectrumSet.create_spectrum_set(create_test_spectra(1, nr_of_inchikeys=3))
+    library_spectra.add_embeddings(model)
+    test_spectra.add_embeddings(model)
+    calculate_MS2DeepScoresForTopKInChikeys_from_spectra(library_spectra, test_spectra, "daylight", 100, 3, 3)
