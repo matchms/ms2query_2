6f96d7631d35242b55476ce916952a74d551608c
niekdejonge <niekdejonge@gmail.com>
2026-03-19 10:56:11 +0100

    Remove predict highest ms2deepscore

--- tests/test_benchmarking/test_methods.py
+++ tests/test_benchmarking/test_methods.py
@@ -1,27 +1,9 @@
 import numpy as np
-import pytest
 from ms2query.benchmarking.Fingerprints import Fingerprints
 from ms2query.benchmarking.reference_methods.predict_best_possible_match import predict_best_possible_match
-from ms2query.benchmarking.reference_methods.predict_highest_ms2deepscore import predict_highest_ms2deepscore
 from tests.helper_functions import (
-    get_library_and_test_spectra_exactly_matching,
     get_library_and_test_spectra_not_identical,
 )
-
-
-@pytest.mark.parametrize(
-    "prediction_function",
-    [
-        predict_highest_ms2deepscore,
-    ],
-)
-def test_all_methods(prediction_function):
-    library_spectra, test_spectra = get_library_and_test_spectra_exactly_matching()
-    predicted_inchikeys, scores = prediction_function(library_spectra, test_spectra)
-    for i, spectrum in enumerate(test_spectra.spectra):
-        inchikey = spectrum.get("inchikey")[:14]  # type: ignore
-        assert predicted_inchikeys[i] == inchikey
-        assert np.allclose(scores[i], np.array(1.0), atol=1e-5)
 
 
 def test_predict_best_possible_match():
